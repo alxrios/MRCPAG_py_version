e350bd45abfacfd8f58e7a6fd61b915465a53f89
alxrios <riosalejandro4238@gmail.com>
2025-11-01 20:01:16 +0100

    print book function added

--- book_class.py
+++ book_class.py
@@ -2,4 +2,22 @@
 class bookClass:
     def __init__(self, title = "", current_page = 0, total_pages = 0):
         self.title = title
+        self.current_page = current_page
+        self.total_pages = total_pages
         
+    def print_book(self):
+        print(self.title)
+        print(self.current_page)
+        print(self.total_pages)
+        print("----------------------------------")
+        print("Title: ", self.title)
+        print("Current page: ", self.current_page)
+        print("Total number of pages: ", self.total_pages)
+        print("----------------------------------")
+        
+        
+        
+        
+        
+test = bookClass("myBook", 5, 100)
+test.print_book()
--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-# test
+
+if __name__ == "__main__":
+    print("hello world")
