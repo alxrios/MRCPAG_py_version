ac1dd8a289b3cad985086f59105d0debfd917c07
alxrios <riosalejandro4238@gmail.com>
2025-11-03 21:55:28 +0100

    shelf class created

---
+++ shelf_class.py
@@ -0,0 +1,76 @@
+
+import book_class
+
+class shelfClass:
+    def __init__(self, n_elements = 0):
+        if n_elements >= 0:
+            self.__n_elements = n_elements
+        else:
+            self.__n_elements = 0
+        
+        self.__shelf = []
+        for i in range(0, self.__n_elements):
+            self.__shelf.append(book_class.bookClass())
+        
+    def get_n_elements(self):
+        """
+        Returns the number of elements in the shelf.
+
+        Returns
+        -------
+        int
+            Number of elements in the shelf.
+
+        """
+        return self.__n_elements
+    
+    def __set_n_elements(self, new_n_elements):
+        """
+        Actualizes the number of elements accounted in the variable n_elements.
+
+        Parameters
+        ----------
+        new_n_elements : int
+            New number of elements in the shelf.
+
+        Returns
+        -------
+        None.
+
+        """
+        if type(new_n_elements) == int and new_n_elements >= 0:
+            self.__n_elements = new_n_elements
+            
+    
+    def add_book_console(self, b_title = "", b_current_p = 0, b_total_p = 1, b_url = "", append = True, index = 0):
+        """
+        Adds a new book with the data passed through the parameters.
+
+        Parameters
+        ----------
+        b_title : str, optional
+            Book's title. The default is "".
+        b_current_p : int, optional
+            Current page of the book. The default is 0.
+        b_total_p : int, optional
+            Total pages of the book. The default is 1.
+        b_url : str, optional
+            Location in the disk of the book file. The default is "".
+        append : bool, optional
+            If True book is added at the end of the current
+            list, if False it's added in the position . The default is True.
+        index : int, optional
+            Possition to add the new book if parameter append is put to False. 
+            The default is 0.
+
+        Returns
+        -------
+        None.
+
+        """
+        if append:
+            self.__shelf.append(book_class.bookClass(b_title, b_current_p, 
+                                                     b_total_p, b_url))
+            
+            # Actualize the number of elements in the shelf.
+            self.__set_n_elements(self.get_n_elements() + 1)
