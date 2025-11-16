bb4230e713a4606392778e2f0c458aaddb8b0375
alxrios <riosalejandro4238@gmail.com>
2025-11-16 22:56:34 +0100

    deleted unused lines

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-
-# import book_class
-# import shelf_class
-# from menu import menu
 import menu
 
 if __name__ == "__main__":
-    # test = shelf_class.shelfClass()
-    # test.get_n_elements()
-    # test.add_book_console("newBook", 50, 100, "myUrl//")
-    # test.get_n_elements()
-    # test.add_book_console("otherBook", 40, 1000, "anotherUrl//", False, 0)
-    # test.print_shelf()
-    # menu()
-    # test.load_data(4)
-    # test.print_shelf()
-    # test.delete_book(5)
-    # test.print_shelf()
-    # test.add_book("new_book_title", 1, 25, "randomurl", False, 5)
-    # test.write_file(4)
-    # test.print_shelf()
     menu.menuClass().menu_display()
