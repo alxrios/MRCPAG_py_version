c5a5174bd46ec1bc126ce18518eece178e4d7356
alxrios <riosalejandro4238@gmail.com>
2025-11-16 20:49:33 +0100

    option five added

--- menu.py
+++ menu.py
@@ -18,9 +18,9 @@ class menuClass:
     Choose an option:
     1. Print the shelf.
     2. Actualize current page of a book.
-    3. Delete a book.
+    3. Actualize the total number of pages of a book.
     4. Rename a book.
-    5. Actualize the total number of pages of a book.
+    5. Delete a book.
     6. Change file.
     9. End program.
 
@@ -51,6 +51,22 @@ class menuClass:
     
     def __ask_new_current_p(self, total):
         return self.__check_book_index("Introduce the new current page: ", total) + 1
+    
+    def __ask_new_total(self, current_page, book_index):
+        message = "Introduce the new total of pages for the book " + str(book_index + 1) + ": "
+        new_total = input(message)
+        success = False
+        while not success:
+            try:
+                new_total = int(new_total)
+                if new_total >= current_page:
+                    success = True
+                else:
+                    new_total = input(message)
+            except:
+                new_total = input(message)
+                    
+        return new_total
     
     def menu_display(self):
         
@@ -86,8 +102,25 @@ class menuClass:
                 shelf.write_file(option_file)
                 # Presents again the menu with the options
                 option_menu = self.__ask_option_menu()
-            # elif option_menu == 3:
-            #     print("")
+            elif option_menu == 3:
+                book_index = self.__check_book_index("Index of the book to be modified: ", shelf.get_n_elements())
+                new_total = self.__ask_new_total(shelf.get_book_current_p(book_index), book_index)
+                shelf.add_book(shelf.get_book_title(book_index), 
+                               shelf.get_book_current_p(book_index),
+                               new_total,
+                               shelf.get_book_file_path(book_index),
+                               False,
+                               book_index)
+                shelf.write_file(option_file)
+                option_menu = self.__ask_option_menu()
+            elif option_menu == 5:
+                book_index = self.__check_book_index("Index of the book to be deleted: ", shelf.get_n_elements())
+                shelf.delete_book(book_index)
+                shelf.write_file(option_file)
+                option_menu = self.__ask_option_menu()
+
+
+
 
 
 
